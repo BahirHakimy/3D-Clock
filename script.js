14180d1d798521abb1e79d0d9ac39794c1ffb224
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-01-10 22:38:34 +0430

    seprated css and js from HTML

---
+++ script.js
@@ -0,0 +1,74 @@
+const zfill = (num) => (num > 9 ? num : "0" + num);
+function $(query) {
+  return document.querySelector(query);
+}
+
+function flip(target) {
+  const next = document.getElementById(`${target}-next`),
+    panel = document.getElementById(`${target}-panel`),
+    bottom = document.getElementById(`${target}-bottom`);
+  let val =
+    target !== "hour"
+      ? next.textContent - 0 + 1 <= 59
+        ? next.textContent - 0 + 1
+        : 0
+      : next.textContent - 0 + 1 <= 12
+      ? next.textContent - 0 + 1
+      : 1;
+  val =
+    target === "ind" ? (next.textContent === "AM" ? "PM" : "AM") : zfill(val);
+  next.textContent = val;
+  panel.innerHTML = `<div class="parts parts-front">${
+    target !== "ind" ? zfill(val - 1) : val === "AM" ? "PM" : "AM"
+  }</div>
+      <div class="parts parts-back">${val}</div>`;
+  panel.classList.toggle("flip");
+  setTimeout(() => {
+    panel.classList.toggle("flip");
+    bottom.textContent = val;
+    panel.innerHTML = "";
+  }, 600);
+}
+function start() {
+  const date = new Date();
+  const time = {
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds(),
+    ind: date.getHours() >= 12 ? "PM" : "AM",
+  };
+  $("#hour-next").innerText = zfill(
+    time.hour > 12 ? time.hour - 12 : time.hour
+  );
+  $("#hour-bottom").innerText = zfill(
+    time.hour > 12 ? time.hour - 12 : time.hour
+  );
+  $("#minute-next").innerText = zfill(time.minute);
+  $("#minute-bottom").innerText = zfill(time.minute);
+  $("#second-next").innerText = zfill(time.second);
+  $("#second-bottom").innerText = zfill(time.second);
+  $("#ind-next").innerText = time.ind;
+  $("#ind-bottom").innerText = time.ind;
+  setInterval(() => {
+    time.second += 1;
+    if (time.second > 59) {
+      time.second = 0;
+      time.minute += 1;
+      flip("minute");
+    }
+    if (time.minute > 59) {
+      time.minute = 0;
+      time.hour += 1;
+      flip("hour");
+    }
+    if (time.hour > 12) {
+      time.hour = 1;
+    }
+    if (time.hour === 11 && time.minute === 59 && time.second === 59) {
+      time.ind = time.ind === "AM" ? "PM" : "AM";
+      flip("ind");
+    }
+    flip("second");
+  }, 1000);
+}
+start();
